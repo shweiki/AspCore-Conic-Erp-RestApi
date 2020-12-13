01871934522bb0b9968d84f91d479a7a47d97719
Taha Shweiki <tahashweiki@yahoo.com>
2020-12-13 22:26:21 +0200

    Update SuperMarketMoveTablesWithDataToAnotherDataBase.sql

--- Entities/SqlServerQuery/SuperMarketMoveTablesWithDataToAnotherDataBase.sql
+++ Entities/SqlServerQuery/SuperMarketMoveTablesWithDataToAnotherDataBase.sql
@@ -34,12 +34,9 @@
 		DROP TABLE [Conic_Erp].[dbo].[Member] 
 		DROP TABLE [Conic_Erp].[dbo].Account 
 
-
-		DROP TABLE [Conic_Erp].[dbo].[BackUp] 
-		DROP TABLE [Conic_Erp].[dbo].[CompanyInfo] 
 	/*	DROP TABLE [Conic_Erp].[dbo].[Device]
-		DROP TABLE [Conic_Erp].[dbo].[EditorsUser]  */
-		DROP TABLE [Conic_Erp].[dbo].[FileData] 
+		DROP TABLE [Conic_Erp].[dbo].[EditorsUser]  
+		DROP TABLE [Conic_Erp].[dbo].[FileData] */
 		DROP TABLE [Conic_Erp].[dbo].[Oprationsys]
 
 
@@ -56,14 +53,14 @@
 
 
 		select * into [Conic_Erp].[dbo].ActionLog from [SuperMarket].Config.ActionLog
-		/*select * into [Conic_Erp].[dbo].[BackUp] from [SuperMarket].[dbo].[BackUp]*/
+		/*select * into [Conic_Erp].[dbo].[BackUp] from [SuperMarket].[dbo].[BackUp]
 		select * into [Conic_Erp].[dbo].[CompanyInfo] from [SuperMarket].Config.[CompanyInfo]
-	/*	select * into [Conic_Erp].[dbo].[Device]  from [SuperMarket].Config.[Device] 
+		select * into [Conic_Erp].[dbo].[Device]  from [SuperMarket].Config.[Device] 
 		select * into [Conic_Erp].[dbo].[EditorsUser] from [SuperMarket].[dbo].[EditorsUser]
 		select * into [Conic_Erp].[dbo].[FileData] from [SuperMarket].Config.[FileData]*/
 		select * into [Conic_Erp].[dbo].[Oprationsys] from [SuperMarket].Config.[Oprationsys]
 
-		/*select * into [Conic_Erp].[dbo].[Member] from [SuperMarket].[Gym].[Member]
+		/*select * into [Conic_Erp].[dbo].[Member] from [SuperMarket].[dbo].[Member]
 	   select * into [Conic_Erp].[dbo].[MemberFace] from [SuperMarket].[dbo].[MemberFace]
 		select * into [Conic_Erp].[dbo].[MemberLog] from [SuperMarket].[dbo].[MemberLog]
 	    select * into [Conic_Erp].[dbo].[Membership] from [SuperMarket].[dbo].[Membership]
@@ -92,3 +89,118 @@
 		ALTER TABLE [Conic_Erp].[dbo].Account ALTER COLUMN Type nvarchar(MAX)
 		ALTER TABLE [Conic_Erp].[dbo].Vendor ALTER COLUMN Description nvarchar(MAX)  
 		ALTER TABLE [Conic_Erp].[dbo].Vendor ALTER COLUMN Address nvarchar(MAX)  
+
+use Conic_Erp
+CREATE TABLE [dbo].[Member](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](max) NOT NULL,
+	[DateofBirth] [date] NULL,
+	[Email] [varchar](255) NULL,
+	[PhoneNumber1] [varchar](13) NULL,
+	[PhoneNumber2] [varchar](13) NULL,
+	[Description] [text] NULL,
+	[Status] [int] NOT NULL,
+	[Type] [text] NOT NULL,
+	[AccountID] [bigint] NOT NULL,
+	[SSN] [varchar](50) NOT NULL,
+	[Tag] [varchar](255) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Membership](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](255) NOT NULL,
+	[NumberDays] [int] NOT NULL,
+	[MorningPrice] [float] NOT NULL,
+	[FullDayPrice] [float] NOT NULL,
+	[Tax] [float] NULL,
+	[Rate] [float] NULL,
+	[Description] [varchar](255) NULL,
+	[Status] [int] NOT NULL,
+	[MinFreezeLimitDays] [int] NULL,
+	[MaxFreezeLimitDays] [int] NULL
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[MembershipMovement](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[TotalAmmount] [float] NOT NULL,
+	[Tax] [float] NULL,
+	[StartDate] [date] NOT NULL,
+	[EndDate] [date] NOT NULL,
+	[Type] [text] NOT NULL,
+	[VisitsUsed] [int] NOT NULL,
+	[Discount] [float] NOT NULL,
+	[Description] [text] NULL,
+	[Status] [int] NOT NULL,
+	[MemberID] [bigint] NOT NULL,
+	[MembershipID] [int] NOT NULL,
+	[DiscountDescription] [varchar](50) NULL,
+	[EditorName] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[MembershipMovementOrder](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [varchar](50) NULL,
+	[StartDate] [date] NOT NULL,
+	[EndDate] [date] NOT NULL,
+	[Status] [int] NOT NULL,
+	[Description] [text] NULL,
+	[EditorName] [varchar](max) NULL,
+	[MemberShipMovementID] [bigint] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[MemberFace](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[FaceLength] [int] NOT NULL,
+	[FaceStr] [nvarchar](max) NOT NULL,
+	[MemberID] [bigint] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[MemberLog](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Type] [text] NOT NULL,
+	[DateTime] [datetime] NOT NULL,
+	[Description] [text] NULL,
+	[Status] [int] NOT NULL,
+	[MemberID] [bigint] NOT NULL,
+	[DeviceID] [bigint] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[Service](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Qty] [int] NOT NULL,
+	[SellingPrice] [float] NOT NULL,
+	[ItemID] [bigint] NOT NULL,
+	[Type] [varchar](50) NOT NULL,
+	[IsPrime] [bit] NOT NULL,
+	[Status] [int] NOT NULL,
+	[Description] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[Payment](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](max) NULL,
+	[FakeDate] [date] NOT NULL,
+	[PaymentMethod] [varchar](255) NOT NULL,
+	[TotalAmmount] [float] NOT NULL,
+	[Description] [text] NULL,
+	[Status] [int] NOT NULL,
+	[VendorID] [bigint] NULL,
+	[IsPrime] [bit] NOT NULL,
+	[MemberID] [bigint] NULL,
+	[Type] [text] NULL,
+	[EditorName] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
