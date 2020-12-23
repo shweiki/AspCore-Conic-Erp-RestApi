4eb16f9d556a3d864bcc1e6549047aabc48569c6
Taha Shweiki <tahashweiki@yahoo.com>
2020-12-23 23:10:26 +0200

    fix

--- AspCore-Conic-Erp-RestApi/Controllers/SettingController.cs
+++ AspCore-Conic-Erp-RestApi/Controllers/SettingController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Entities; 
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace AspCore_Conic_Erp_RestApi.Controllers
 {
@@ -62,22 +63,35 @@ namespace AspCore_Conic_Erp_RestApi.Controllers
             }
             return Ok(false);
         }
+        [Route("Setting/CheckSetting")]
+        [HttpPost]
+        public IActionResult CheckSetting(Setting dynamicRequest)
+        {
+     
+            return Ok(dynamicRequest);
+        }
+
         [Route("Setting/Edit")]
         [HttpPost]
         public IActionResult Edit(Setting collection)
         {
             if (ModelState.IsValid)
             {
-                    Setting Setting = DB.Settings.Where(x => x.Id == collection.Id).SingleOrDefault();
+                Setting Setting = DB.Settings.Where(x => x.Name == collection.Name).SingleOrDefault();
+                if (Setting != null) {
                     Setting.Name = collection.Name;
                     Setting.Status = collection.Status;
                     Setting.Value = collection.Value;
                     Setting.Type = collection.Type;
                     Setting.Description = collection.Description;
+                } else
+                {
+                    DB.Settings.Add(collection);
+                }
                 try
                 {
                     DB.SaveChanges();
-                    return Ok(true);
+                    return Ok(collection);
                 }
                 catch
                 {
