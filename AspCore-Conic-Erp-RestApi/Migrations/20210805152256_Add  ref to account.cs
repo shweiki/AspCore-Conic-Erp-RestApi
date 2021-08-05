d000f3057248ea5342840510e1974d198966e3bb
Taha Shweiki <tahashweiki@yahoo.com>
2021-08-05 19:01:26 +0300

    dsad

---
+++ AspCore-Conic-Erp-RestApi/Migrations/20210805152256_Add  ref to account.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspCore_Conic_Erp_RestApi.Migrations
+{
+    public partial class Addreftoaccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ref",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ref",
+                table: "Account");
+        }
+    }
+}
