8ee22d55318298dcd049a6239e20c8c24dccf882
Taha Shweiki <tahashweiki@yahoo.com>
2022-06-13 23:07:56 +0300

    last

--- AspCore-Conic-Erp-RestApi/Controllers/MembershipMovementController.cs
+++ AspCore-Conic-Erp-RestApi/Controllers/MembershipMovementController.cs
@@ -84,8 +84,7 @@ namespace AspCore_Conic_Erp_RestApi.Controllers
             MS.EndDate = MS.StartDate.AddDays(MembershipNumberDays + TotalMembershipMovementOrders);
             if (DateTime.Now >= MS.StartDate.Date && DateTime.Now <= MS.EndDate.Date.AddHours(23).AddMinutes(59).AddSeconds(59))
                 {
-                var DeviceLogs = DB.DeviceLogs.Where(x => x.Fk == member.Id.ToString() && x.TableName == "Member" && (x.DateTime >= MS.StartDate.Date && x.DateTime <= MS.EndDate.Date.AddHours(23).AddMinutes(59).AddSeconds(59))).ToList()
-            ;
+                var DeviceLogs = DB.DeviceLogs.Where(x => x.Fk == member.Id.ToString() && x.TableName == "Member" && (x.DateTime >= MS.StartDate.Date && x.DateTime <= MS.EndDate.Date.AddHours(23).AddMinutes(59).AddSeconds(59))).ToList();
                 if (DeviceLogs !=null  &&DeviceLogs.Count > 0) {
                     DeviceLogs.GroupBy(a => a.DateTime).Select(g => g.Last()).ToList();
                     MS.VisitsUsed = DeviceLogs.Count();
